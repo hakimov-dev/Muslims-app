d53008df019bca4a485c8147e7e33d1b2891d2eb
Muhammadamin <webhacker0100@gmail.com>
2022-08-13 15:59:35 +0500

    Added vue-router

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import store from "./store";
+import router from "./router";
 
-createApp(App).use(store).mount("#app");
+createApp(App).use(router).use(store).mount("#app");
