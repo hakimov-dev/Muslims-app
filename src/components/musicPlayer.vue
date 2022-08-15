7eaaa6eaa2a61c62cd7bb45da0865aed38e9269c
Muhammadamin <webhacker0100@gmail.com>
2022-08-16 00:06:05 +0500

    Added formatTime function content

--- src/components/musicPlayer.vue
+++ src/components/musicPlayer.vue
@@ -57,7 +57,12 @@ function onPlay(){
 }
 
 function formatTime(audio_duration){
- 
+ let sec = Math.floor( audio_duration );
+ let min = Math.floor( sec / 60 );
+  min = min >= 10 ? min : '0' + min;
+  sec = Math.floor( sec % 60 );
+  sec = sec >= 10 ? sec : '0' + sec;
+  return min + ":"+ sec;
 }
 </script>
 
