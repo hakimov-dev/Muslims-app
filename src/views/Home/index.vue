c0daf81916fbf04bcab06dc5b9d20ee0ba98b810
Muhammadamin <webhacker0100@gmail.com>
2022-08-17 17:13:49 +0500

    Added card component in template

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="main-page">
         <div class="cards flex justify-between items-center flex-wrap md:p-[10%]">
-         
+         <Card />
         </div>
     </div>
 </template>
 
 <script setup>
-import card from './components/mp3Card.vue'
+import Card from './components/mp3Card.vue'
 </script>
