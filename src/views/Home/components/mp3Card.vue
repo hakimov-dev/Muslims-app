0ba2c9aed7d9f5748ffebe2f7fa190ad5a07afef
Muhammadamin <webhacker0100@gmail.com>
2022-08-17 23:08:54 +0500

    Added styles for responsive design.

--- src/views/Home/components/mp3Card.vue
+++ src/views/Home/components/mp3Card.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="card cursor-pointer group transition-all hover:bg-[#282828] bg-[#171717] p-[10px] w-[220px] h-[310px] shadow-xl md:my-[20px] rounded-xl">
-       <div class="img-wrap flex flex-col h-[160px] overflow-hidden relative">
-         <img class="h-[160px] !rounded-xl shadow-xl" src="https://kartinkin.net/uploads/posts/2021-07/1626954887_7-kartinkin-com-p-koran-fon-krasivo-7.jpg" alt="">
+    <div class="card cursor-pointer group transition-all hover:bg-[#282828] bg-[#171717] md:p-[10px] p-[6px] md:w-[220px] md:h-[310px] w-[175px] h-[250px] shadow-xl my-[20px] rounded-xl">
+       <div class="img-wrap flex flex-col md:h-[160px] h-[120px] overflow-hidden relative">
+         <img class="md:h-[160px] h-[120px] !rounded-xl shadow-xl" src="https://kartinkin.net/uploads/posts/2021-07/1626954887_7-kartinkin-com-p-koran-fon-krasivo-7.jpg" alt="">
          <img class="group-hover:top-[70%] transition-all h-[40px] w-[40px] p-[10px] rounded-full bg-white absolute top-[120%] left-[75%]" src="../../../assets/imgs/play.png" alt="">
        </div>
        <div class="text-wrap flex flex-col p-3 text-white leading-[30px]">
