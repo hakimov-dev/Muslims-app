dbfe4613908caac96dbf28d232d1bf631e033f87
Muhammadamin <webhacker0100@gmail.com>
2022-08-17 22:55:59 +0500

    Added text wrap and added some content in text-wrap

--- src/views/Home/components/mp3Card.vue
+++ src/views/Home/components/mp3Card.vue
@@ -4,6 +4,11 @@
          <img class="h-[160px] !rounded-xl shadow-xl" src="https://kartinkin.net/uploads/posts/2021-07/1626954887_7-kartinkin-com-p-koran-fon-krasivo-7.jpg" alt="">
          <img class="group-hover:top-[70%] transition-all h-[40px] w-[40px] p-[10px] rounded-full bg-white absolute top-[120%] left-[75%]" src="../../../assets/imgs/play.png" alt="">
        </div>
+       <div class="text-wrap flex flex-col p-3 text-white leading-[30px]">
+         <h1 class="mp3-name text-[18px] font-medium">Baqarah</h1>
+         <h1 class="mp3-author text-[14px] opacity-60">Alafsy</h1>
+         <h1 class="ayah-number text-[14px] opacity-60">000</h1>
+       </div>
     </div>
 </template>
 
