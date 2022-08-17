36fbb0f0ee2c7c45fb20e7372773e51e16cd81c1
Muhammadamin <webhacker0100@gmail.com>
2022-08-17 22:47:17 +0500

    Update classes and added group hover animation.

--- src/views/Home/components/mp3Card.vue
+++ src/views/Home/components/mp3Card.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="card bg-[#171717] p-[10px] w-[220px] h-[310px] shadow-xl md:my-[20px] rounded-[3px]">
-       <div class="img-wrap">
-         <img class="md:h-[160px] !rounded-xl shadow-xl" src="https://kartinkin.net/uploads/posts/2021-07/1626954887_7-kartinkin-com-p-koran-fon-krasivo-7.jpg" alt="">
+    <div class="card cursor-pointer group transition-all hover:bg-[#282828] bg-[#171717] p-[10px] w-[220px] h-[310px] shadow-xl md:my-[20px] rounded-xl">
+       <div class="img-wrap flex flex-col h-[160px] overflow-hidden relative">
+         <img class="h-[160px] !rounded-xl shadow-xl" src="https://kartinkin.net/uploads/posts/2021-07/1626954887_7-kartinkin-com-p-koran-fon-krasivo-7.jpg" alt="">
+         <img class="group-hover:top-[70%] transition-all h-[40px] w-[40px] p-[10px] rounded-full bg-white absolute top-[120%] left-[75%]" src="../../../assets/imgs/play.png" alt="">
        </div>
     </div>
 </template>
