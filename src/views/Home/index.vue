070f82b5b27a28683ef535a3af864a5a73422062
Muhammadamin <webhacker0100@gmail.com>
2022-08-18 00:07:54 +0500

    Added input for search surah

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="main-page">
+    <input class="md:hidden inline search outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 w-[100%] p-2 px-10 bg-[#121416] rounded-[20px]" type="text" placeholder="Search surah...">
         <div class="cards flex justify-between items-center flex-wrap md:p-[3%]">
          <Card v-for="item in 114"/>
         </div>
