513152f1ef08d306499632a60eff23b2289f7b76
Muhammadamin <webhacker0100@gmail.com>
2022-08-18 11:51:37 +0500

    Update date variable value

--- src/views/Calendar/index.vue
+++ src/views/Calendar/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="muslims-calendar md:p-[3%] md:pt-[1%]"> 
         <!-- Date picker -->
-        <input v-model="date" class="p-1 px-3 outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 p-2 px-10 bg-[#121416] rounded-[10px] cursor-pointer" type="date" name="date" id="date">
+        <input v-model="date" class="p-1 px-3 text-white outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 p-2 px-10 bg-[#121416] rounded-[10px] cursor-pointer" type="date" name="date" id="date">
     </div>
 </template>
 
@@ -12,6 +12,7 @@ let date = ref('')
 
 const newDate = new Date()
 
+date.value = `${newDate.getFullYear()}-${String(newDate.getMonth() + 1).padStart(2, '0')}-${String(newDate.getDate()).padStart(2, '0')}`
 </script>
 
 <style scoped>
