0edac96d3589b819cfe174bd226a48bd7f3c922b
Muhammadamin <webhacker0100@gmail.com>
2022-08-18 09:31:00 +0500

    Added function in image src

--- src/views/PrayTimes/components/PrayTimeCard.vue
+++ src/views/PrayTimes/components/PrayTimeCard.vue
@@ -1,7 +1,7 @@
 <template>
-     <div class="pray-time-card cursor-pointer group transition-all hover:bg-[#282828] bg-[#171717] md:p-[10px] p-[6px] md:w-[220px] md:h-[310px] w-[175px] h-[250px] shadow-xl my-[20px] rounded-xl">
+     <div class="pray-time-card cursor-pointer group transition-all hover:bg-[#282828] bg-[#171717] md:p-[10px] p-[6px] md:w-[245px] md:h-[310px] w-[175px] h-[250px] shadow-xl m-[2%] rounded-xl">
        <div class="img-wrap flex flex-col md:h-[160px] h-[120px] overflow-hidden relative">
-         <img class="md:h-[160px] h-[120px] !rounded-xl shadow-xl" src="https://kartinkin.net/uploads/posts/2021-07/1626954887_7-kartinkin-com-p-koran-fon-krasivo-7.jpg" alt="">
+         <img class="md:h-[160px] h-[120px] !rounded-xl shadow-xl" :src="returnImage(props.id)" alt="">
        </div>
        <div class="text-wrap flex flex-col p-3 text-white leading-[30px]">
          <h1 class="pray-name text-[18px] font-medium"></h1>
