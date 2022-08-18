388813aa7ba870edc635dbaecc0d8c6c198e9581
Muhammadamin <webhacker0100@gmail.com>
2022-08-18 09:27:07 +0500

    Update v-for numbers

--- src/views/PrayTimes/index.vue
+++ src/views/PrayTimes/index.vue
@@ -2,7 +2,7 @@
     <div class="pray-times">
         <input class="md:w-[60%] md:mx-auto md:flex justify-center search outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 w-[100%] p-2 px-10 bg-[#121416] rounded-[20px]" type="text" placeholder="Search city...">
         <div class="pray-times-cards flex justify-evenly items-center flex-wrap mt-[3%]">
-          <PrayTimeCard v-for="item in 7" :id="item"/>
+          <PrayTimeCard v-for="item in 6" :id="item"/>
         </div>
     </div>
 </template>
