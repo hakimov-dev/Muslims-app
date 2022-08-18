f772d9437f4c3603de7b5aa52454cdd378f2095f
Muhammadamin <webhacker0100@gmail.com>
2022-08-18 10:57:59 +0500

    Added new date input

--- src/views/Calendar/index.vue
+++ src/views/Calendar/index.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="muslims-calendar">
-        <h1 class="text-2xl font-medium text-white">This is muslims-calendar page</h1>
+    <div class="muslims-calendar md:p-[3%] md:pt-[1%]"> 
+        <!-- Date picker -->
+        <input class="p-1 px-3 outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 p-2 px-10 bg-[#121416] rounded-[10px] cursor-pointer" type="date" name="date" id="date">
     </div>
 </template>
