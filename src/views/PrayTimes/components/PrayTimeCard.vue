2d02c21179fad53a770962cf16fd03f586faeee3
Muhammadamin <webhacker0100@gmail.com>
2022-08-18 09:04:39 +0500

    Added main content in card

---
+++ src/views/PrayTimes/components/PrayTimeCard.vue
@@ -0,0 +1,11 @@
+<template>
+     <div class="pray-time-card cursor-pointer group transition-all hover:bg-[#282828] bg-[#171717] md:p-[10px] p-[6px] md:w-[220px] md:h-[310px] w-[175px] h-[250px] shadow-xl my-[20px] rounded-xl">
+       <div class="img-wrap flex flex-col md:h-[160px] h-[120px] overflow-hidden relative">
+         <img class="md:h-[160px] h-[120px] !rounded-xl shadow-xl" src="https://kartinkin.net/uploads/posts/2021-07/1626954887_7-kartinkin-com-p-koran-fon-krasivo-7.jpg" alt="">
+       </div>
+       <div class="text-wrap flex flex-col p-3 text-white leading-[30px]">
+         <h1 class="pray-name text-[18px] font-medium"></h1>
+         <h1 class="pray-time text-[14px] opacity-60"></h1>
+       </div>
+    </div>
+</template>
