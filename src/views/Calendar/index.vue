738129fcb8fde099a1244174a78d21009cd59993
Muhammadamin <webhacker0100@gmail.com>
2022-08-18 11:16:39 +0500

    Update

--- src/views/Calendar/index.vue
+++ src/views/Calendar/index.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="muslims-calendar md:p-[3%] md:pt-[1%]"> 
         <!-- Date picker -->
-        <input class="p-1 px-3 outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 p-2 px-10 bg-[#121416] rounded-[10px] cursor-pointer" type="date" name="date" id="date">
+        <input v-model="date" class="p-1 px-3 outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 p-2 px-10 bg-[#121416] rounded-[10px] cursor-pointer" type="date" name="date" id="date">
     </div>
 </template>
 
 <script setup>
- const date = ''
+import { ref } from 'vue'
+
+ let date = ref('')
+
 </script>
 
 <style scoped>
