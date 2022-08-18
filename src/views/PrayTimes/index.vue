100b28a23170d4473e9f389e064524f9b0a780a8
Muhammadamin <webhacker0100@gmail.com>
2022-08-18 08:48:41 +0500

    Added input

--- src/views/PrayTimes/index.vue
+++ src/views/PrayTimes/index.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="pray-times">
-        
+        <input class="inline search outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 w-[100%] p-2 px-10 bg-[#121416] rounded-[20px]" type="text" placeholder="Search city...">
     </div>
 </template>
