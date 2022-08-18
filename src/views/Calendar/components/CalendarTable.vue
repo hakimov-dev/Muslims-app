d3ea02f9afc0cb517b13e66e40fabf238bc4154c
Muhammadamin <webhacker0100@gmail.com>
2022-08-18 14:03:14 +0500

    Update table content

--- src/views/Calendar/components/CalendarTable.vue
+++ src/views/Calendar/components/CalendarTable.vue
@@ -1,68 +1,73 @@
 <template>
-    <div>
-        <div class="flex flex-col">
-  <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-    <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-      <div class="overflow-hidden">
-        <table class="min-w-full">
-          <thead class="border-b">
-            <tr>
-              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                #
-              </th>
-              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                First
-              </th>
-              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                Last
-              </th>
-              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                Handle
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr class="border-b">
-              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                Mark
-              </td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                Otto
-              </td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                @mdo
-              </td>
-            </tr>
-            <tr class="bg-white border-b">
-              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">2</td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                Jacob
-              </td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                Thornton
-              </td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                @fat
-              </td>
-            </tr>
-            <tr class="bg-white border-b">
-              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">3</td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                Larry
-              </td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                Wild
-              </td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                @twitter
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
+   <div class="bg-white rounded-xl">
+
+<div class="overflow-x-auto !rounded-t-xl mt-[3%]">
+   <table class="table-auto w-full !rounded-t-xl">
+      <thead class="border-b">
+         <tr class="bg-gray-100">
+            <th class="text-center p-4 font-medium">
+               Mуxаrrам
+            </th>
+            <th class="text-center border-l-[1px] border-gray-400 p-4 font-medium">
+               August
+            </th>
+            <th class="text-center border-l-[1px] border-gray-400 p-4 font-medium">
+               Weekday
+            </th>
+            <th class="text-center border-l-[1px] border-gray-400 p-4 font-medium">
+               Fajr
+            </th>
+            <th class="text-center border-l-[1px] border-gray-400 p-4 font-medium">
+               Sunrise
+            </th>
+            <th class="text-center border-l-[1px] border-gray-400 p-4 font-medium">
+               Dhuhr
+            </th>
+            <th class="text-center border-l-[1px] border-gray-400 p-4 font-medium">
+               Asr
+            </th>
+            <th class="text-center border-l-[1px] border-gray-400 p-4 font-medium">
+               Maghrib
+            </th>
+            <th class="text-center border-l-[1px] border-gray-400 p-4 font-medium">
+               Isha
+            </th>
+         </tr>
+      </thead>
+
+      <!-- Content -->
+      <tbody>
+         <tr class="!rounded-b-xl">
+            <td class="p-4 text-center">
+               3 
+            </td>
+            <td class="p-4 text-center border-l-[1px] border-gray-400">
+               1 
+            </td>
+            <td class="p-4 text-center border-l-[1px] border-gray-400">
+               Monday
+            </td>
+            <td class="p-4 text-center border-l-[1px] border-gray-400">
+               03:48 (+05)
+            </td>
+            <td class="p-4 text-center border-l-[1px] border-gray-400">
+               05:18 (+05)
+            </td>
+            <td class="p-4 text-center border-l-[1px] border-gray-400">
+               12:29 (+05)
+            </td>
+            <td class="p-4 text-center border-l-[1px] border-gray-400">
+               16:25 (+05)
+            </td>
+            <td class="p-4 text-center border-l-[1px] border-gray-400">
+               19:40 (+05)
+            </td>
+            <td class="p-4 text-center border-l-[1px] border-gray-400">
+               21:10 (+05)
+            </td>
+         </tr>
+      </tbody>
+   </table>
 </div>
-    </div>
+</div>
 </template>
