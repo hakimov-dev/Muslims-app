2dd5262e3a685533cd2b28e00f1d1da4ec7167f3
Muhammadamin <webhacker0100@gmail.com>
2022-08-18 08:56:29 +0500

    Added new content

--- src/views/PrayTimes/index.vue
+++ src/views/PrayTimes/index.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="pray-times">
         <input class="md:w-[60%] md:mx-auto md:flex justify-center search outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 w-[100%] p-2 px-10 bg-[#121416] rounded-[20px]" type="text" placeholder="Search city...">
+        <div class="pray-times-cards flex justify-evenly items-center">
+          
+        </div>
     </div>
 </template>
