20208ab3092aaa9798063108775c3079511404d0
Muhammadamin <webhacker0100@gmail.com>
2022-08-18 13:21:55 +0500

    Added CalendarTable component in template

--- src/views/Calendar/index.vue
+++ src/views/Calendar/index.vue
@@ -2,6 +2,8 @@
     <div class="muslims-calendar md:p-[3%] md:pt-[1%]"> 
         <!-- Date picker -->
         <input v-model="date" class="p-1 px-3 text-white outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 p-2 px-10 bg-[#121416] rounded-[10px] cursor-pointer" type="date" name="date" id="date">
+
+        <CalendarTable />
     </div>
 </template>
 
