5b4b3b1e629a5b3297a32d694c704421e3495aff
Muhammadamin <webhacker0100@gmail.com>
2022-08-18 10:59:23 +0500

    Added style

--- src/views/Calendar/index.vue
+++ src/views/Calendar/index.vue
@@ -4,3 +4,9 @@
         <input class="p-1 px-3 outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 p-2 px-10 bg-[#121416] rounded-[10px] cursor-pointer" type="date" name="date" id="date">
     </div>
 </template>
+
+<style scoped>
+ input{
+    width: 100%;
+ }
+</style>
