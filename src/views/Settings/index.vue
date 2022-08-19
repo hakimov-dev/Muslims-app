8db2ec161b3887016ff9cc2f754dcdf23d3b6d38
Muhammadamin <webhacker0100@gmail.com>
2022-08-19 22:33:24 +0500

    Update inputs wrap width

--- src/views/Settings/index.vue
+++ src/views/Settings/index.vue
@@ -6,11 +6,12 @@
          <div class="img-wrap flex justify-center items-center text-center">
           <img class="md:h-[120px] md:w-[120px] rounded-full object-contain bg-black p-3 select-none" src="../../assets/imgs/user2.png" alt="">
          </div>
-         <div class="inputs-wrap mt-[20px] w-[80%]">
+         <div class="inputs-wrap mt-[20px] w-[70%]">
           <input class="md:flex justify-center outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 w-[100%] p-2 px-4 bg-[#121416] rounded-[5px]" type="text" placeholder="Enter your name...">
           <input class="md:flex my-[30px] justify-center outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 w-[100%] p-2 px-4 bg-[#121416] rounded-[5px]" type="text" placeholder="Enter your name...">
           <input class="md:flex justify-center outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 w-[100%] p-2 px-4 bg-[#121416] rounded-[5px]" type="text" placeholder="Enter your name...">
          </div>
+
         </div>
     </div>
 </template>
