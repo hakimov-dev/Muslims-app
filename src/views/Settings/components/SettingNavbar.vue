3af7667b37e8254c82c7b0b429be295b22f60cc0
Muhammadamin <webhacker0100@gmail.com>
2022-08-19 16:06:42 +0500

    Added main content in template

---
+++ src/views/Settings/components/SettingNavbar.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="settings-navbar">
+        <div class="desktop-nav flex w-[30%] justify-evenly items-center">
+          <router-link to="/user-settings">User settings</router-link>
+          <router-link to="/update-passwords">Update password</router-link>
+        </div>
+    </div>
+</template>
