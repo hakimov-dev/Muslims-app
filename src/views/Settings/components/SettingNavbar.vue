f23d7b3ee471b1aab00a126d00cc4683cb753a13
Muhammadamin <webhacker0100@gmail.com>
2022-08-19 16:09:35 +0500

    Update classes

--- src/views/Settings/components/SettingNavbar.vue
+++ src/views/Settings/components/SettingNavbar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="settings-navbar">
-        <div class="desktop-nav flex w-[30%] justify-evenly items-center">
+        <div class="desktop-nav flex w-[30%] justify-evenly items-center text-white font-medium text-[18px] pb-2">
           <router-link to="/user-settings">User settings</router-link>
           <router-link to="/update-passwords">Update password</router-link>
         </div>
