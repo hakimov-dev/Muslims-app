cbc56a349b20aa3ecb1cb71c97116bc7abe1bb65
Muhammadamin <webhacker0100@gmail.com>
2022-08-19 23:15:08 +0500

    Added v-ifs

--- src/views/Settings/index.vue
+++ src/views/Settings/index.vue
@@ -2,7 +2,7 @@
     <div class="user-settings md:p-[3%] md:pt-[1%]">
         <SettingNav />
         <!-- User settings content -->
-        <div class="flex flex-col items-center md:w-[50%] mt-[3%]">
+        <div v-if="$route.name == 'user-settings'" class="flex flex-col items-center md:w-[50%] mt-[3%]">
          <div class="img-wrap flex justify-center items-center text-center">
           <img class="md:h-[120px] md:w-[120px] rounded-full object-contain bg-black p-3 select-none" src="../../assets/imgs/user2.png" alt="">
          </div>
@@ -20,7 +20,7 @@
         >
         </div>
         <!-- Update password page -->
-         <div class="flex flex-col items-center md:w-[50%] mt-[3%]">
+         <div v-if="$route.name == 'update-pasword'" class="flex flex-col items-center md:w-[50%] mt-[3%]">
          <div class="inputs-wrap mt-[20px] md:w-[70%] w-[80%]">
           <input class="md:flex justify-center outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 w-[100%] p-2 px-4 bg-[#121416] rounded-[5px]" type="text" placeholder="Enter your old password">
           <input class="md:flex my-[30px] justify-center outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 w-[100%] p-2 px-4 bg-[#121416] rounded-[5px]" type="text" placeholder="Enter your new password">
