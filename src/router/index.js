d3c227f043e31ae248ced385846653921997b149
Muhammadamin <webhacker0100@gmail.com>
2022-08-20 14:29:24 +0500

    Added meta layouts

--- src/router/index.js
+++ src/router/index.js
@@ -4,36 +4,57 @@ const routes = [
   {
     path: "/",
     name: "home",
+    meta: {
+      layout: 'main'
+    },
     component: () => import("../views/Home")
   },
   {
     path: "/pray-time",
     name: "pray-time",
+    meta: {
+      layout: 'main'
+    },
     component: () => import("../views/PrayTimes")
   },
   {
     path: "/muslims-calendar",
     name: "muslims-calendar",
+    meta: {
+      layout: 'main'
+    },
     component: () => import("../views/Calendar")
   },
   {
     path: "/user-settings",
     name: "user-settings",
+    meta: {
+      layout: 'main'
+    },
     component: () => import("../views/Settings")
   },
   {
     path: "/update-passwords",
     name: "update-pasword",
+    meta: {
+      layout: 'main'
+    },
     component: () => import("../views/Settings")
   },
   {
     path: '/search-result/search/:query',
     name: "search-result",
+    meta: {
+      layout: 'main'
+    },
     component: () => import("../views/Search")
   },
   {
      path: '/favorite',
      name: 'favorite',
+     meta: {
+      layout: 'main'
+     },
      component: () => import("../views/Favorite")
   }
 ];
