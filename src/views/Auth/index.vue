4d41cdccb61a378777e4f9ef257c0db6e581dd46
Muhammadamin <webhacker0100@gmail.com>
2022-08-20 17:18:38 +0500

    Added v-if for error text

--- src/views/Auth/index.vue
+++ src/views/Auth/index.vue
@@ -27,7 +27,7 @@
           />
 
          <transition>
-          <h1 class="text-red-600 text-[16px] mt-[3%]">The login or password is incorrect. Please check and try again!</h1>
+          <h1 class="text-red-600 text-[16px] mt-[3%]" v-if="$store.state.auth.isError">The login or password is incorrect. Please check and try again!</h1>
          </transition>
           <button
           class="mt-[5%] uppercase border-[1px] text-white transition-all font-medium hover:border-gray-300 border-gray-500 w-[70%] p-2 px-4 bg-[#121416] rounded-[5px]"
