aaabbdefef2a09e7ae6223aaf54db19c22d231a1
Muhammadamin <webhacker0100@gmail.com>
2022-08-20 15:31:54 +0500

    Update layout

--- src/layouts/authLayout.vue
+++ src/layouts/authLayout.vue
@@ -1,5 +1,30 @@
 <template>
-    <div class="auth-layout">
-        <h1>This is auth layout page</h1>
+    <div class="auth-layout flex items-center">
+        <div class="img-wrap md:block hidden w-[70%] text-center">
+           <router-link
+           to="/"
+           class="logo w-[20%] absolute z-[111] md:mt-[1%] select-none text-center md:flex items-center justify-center hidden py-4 w-full"
+         >
+          <img
+            class="h-[40px] w-[40px] mr-2"
+            src="../assets/imgs/logo.png"
+            alt=""
+          />
+           <h1 class="font-medium text-white text-2xl">Muslims app</h1>
+          </router-link>
+          <img class="relative w-full h-screen object-fill pointer-events-none" src="../assets/imgs/mosque.jpg" alt="">
+        </div>
     </div>
 </template>
+
+<style scoped>
+.img-wrap::after {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 70%;
+  height: 100%;
+  background: #0e0b0b4b;
+  }
+</style>
