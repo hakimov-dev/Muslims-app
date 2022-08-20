ccfd227ca06150e008c5eb7ca9306f38265eb952
Muhammadamin <webhacker0100@gmail.com>
2022-08-20 17:43:47 +0500

    Added cookies items

--- src/store/Auth/index.js
+++ src/store/Auth/index.js
@@ -12,6 +12,12 @@ export default{
             if(state.loginValue == 'hakimov-dev' && state.passwordValue == 'hakimovDev'){
                 state.isError = false
                 router.push('/')
+                $cookies.set('user_username', 'hakimov-dev')
+                $cookies.set('user_name', 'Muhammadamin')
+                $cookies.set('user_lastname', 'Hakimov')
+                $cookies.set('user_pic', null)
+                $cookies.set('user_pass', state.passwordValue) 
+                $cookies.set('user_favorite', JSON.stringify([]))
             }else
                 state.isError = true
         }
