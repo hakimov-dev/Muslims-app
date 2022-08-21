9ee6c9247e7c7bdd0b4c4ecd9c42e11af5f899a2
Muhammadamin <webhacker0100@gmail.com>
2022-08-21 21:23:42 +0500

    Update Loading card component content.

--- src/views/Home/components/loadingCard.vue
+++ src/views/Home/components/loadingCard.vue
@@ -1,24 +1,20 @@
 <template>
 <div
-    class="card cursor-pointer group transition-all hover:bg-[#282828] bg-[#171717] md:p-[10px] p-[6px] md:w-[240px] md:h-[310px] w-[175px] h-[250px] shadow-xl my-[20px] rounded-xl"
+    class="card cursor-pointer group transition-all bg-[#171717] md:p-[10px] p-[6px] md:w-[240px] md:h-[310px] w-[175px] h-[250px] shadow-xl my-[20px] rounded-xl"
   >
     <div
       class="img-wrap flex flex-col md:h-[160px] h-[120px] overflow-hidden relative"
     >
-      <div class="md:h-[160px] h-[120px] !rounded-xl shadow-xl bg-[#c4c4c4] select-none text-center flex items-center justify-center">
-       <h1 class="text-[#00000] font-semibold image-text text-[22px]">Loading</h1>
+      <div class="md:h-[160px] h-[120px] !rounded-xl shadow-xl bg-gray-300 animate-pulse select-none text-center flex items-center justify-center">
+       <h1 class="text-[#00000] font-semibold image-text text-[22px] hidden">Loading</h1>
       </div>
     </div>
-    <div class="text-wrap flex flex-col p-3 text-white leading-[30px]">
-      <h1 class="mp3-name text-[18px] font-medium flex items-center w-full">
+    <div class="text-wrap flex flex-col p-3 text-white leading-[40px]">
+      <h1 class="mp3-name bg-gray-300 animate-pulse text-gray-300 rounded-[3px] text-[18px] font-medium flex items-center w-full">
         Loading name
       </h1>
-      <h1 class="mp3-author text-[14px] opacity-60">Alafasy</h1>
-      <h1 class="ayah-number text-[14px] opacity-60">
+      <h1 class="ayah-number text-[14px] mt-[20px] opacity-60 w-[60%] bg-gray-300 animate-pulse text-gray-300 rounded-[3px]">
         load ayahs
-      </h1>
-      <h1 class="revelation-type text-[14px] opacity-60 md:block hidden">
-        Revelation type: loading
       </h1>
     </div>
   </div>
