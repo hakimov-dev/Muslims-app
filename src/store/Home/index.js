f5fb663ed471f8134b7c8841bc47306b4d712a6c
Muhammadamin <webhacker0100@gmail.com>
2022-08-21 16:56:48 +0500

    Created new playerContent state variable

--- src/store/Home/index.js
+++ src/store/Home/index.js
@@ -4,7 +4,8 @@ export default {
   namespaced: true,
   state: {
     surahList: null,
-    activaSurahID: 0
+    activaSurahID: 0,
+    playerContent: null
   },
   mutations: {
     setSurahList(state, payload) {
