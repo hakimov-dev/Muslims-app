30293f57f5ad8729e3d617f3e2d34aef6585704d
Muhammadamin <webhacker0100@gmail.com>
2022-08-21 08:45:59 +0500

    Update and added new loader

--- src/layouts/authLayout.vue
+++ src/layouts/authLayout.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="auth-layout flex items-center">
+    <div class="auth-layout flex items-center" v-if="$route.name == 'auth' || $route.name == 'login'">
         <div class="img-wrap md:block hidden w-[70%] text-center">
            <router-link
            to="/"
@@ -16,6 +16,9 @@
         </div>
        <router-view />
     </div>
+    <section class="loading bg-[#121415] h-screen w-full overflow-hidden relative text-center flex items-center justify-center" v-else>
+      <span class="loader"></span>
+    </section>
 </template>
 
 <style scoped>
@@ -27,5 +30,45 @@
   width: 70%;
   height: 100%;
   background: #0e0b0b4b;
+  }
+
+  .loader {
+    width: 48px;
+    height: 48px;
+    display: inline-block;
+    position: relative;
+  }
+  .loader::after, .loader::before {
+    content: "";
+    width: 48px;
+    height: 48px;
+    border: 2px solid #FFF;
+    position: absolute;
+    left: 0;
+    top: 0;
+    -webkit-animation: rotation 2s ease-in-out infinite;
+            animation: rotation 2s ease-in-out infinite;
+  }
+  .loader::after {
+    border-color: #0fff;
+    -webkit-animation-delay: 1s;
+            animation-delay: 1s;
+  }
+
+  @-webkit-keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
   }
 </style>
