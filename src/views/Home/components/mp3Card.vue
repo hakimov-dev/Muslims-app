ff010c58463138c26e70197b1840db69b40cb771
Muhammadamin <webhacker0100@gmail.com>
2022-08-21 15:49:09 +0500

    Update classes

--- src/views/Home/components/mp3Card.vue
+++ src/views/Home/components/mp3Card.vue
@@ -13,8 +13,8 @@
         alt=""
       />
       <img
-        :class="$store.state.home.activaSurahID == surahAbout.number ? '' : 'group-hover:top-[70%]'"
-        class="transition-all h-[40px] w-[40px] p-[10px] rounded-full bg-white absolute top-[120%] left-[75%]"
+        :class="$store.state.home.activaSurahID == surahAbout.number ? '' : ' md:group-hover:top-[70%] group-hover:top-[60%]'"
+        class="transition-all h-[40px] w-[40px] p-[10px] rounded-full bg-white absolute top-[120%] md:left-[75%] left-[70%]"
         src="../../../assets/imgs/play.png"
         alt=""
       />
