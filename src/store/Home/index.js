d483195f7fce8f48fd991ecb5e36ed70b6c8f80d
Muhammadamin <webhacker0100@gmail.com>
2022-08-21 09:38:06 +0500

    Added console

--- src/store/Home/index.js
+++ src/store/Home/index.js
@@ -8,6 +8,8 @@ export default {
      async getAbouts(ctx){
        try{
        const userIP = await axios.get(process.env.VUE_APP_IP_API)
+       
+       console.log(userIP)
        }catch(error){
         console.log(error)
        }
