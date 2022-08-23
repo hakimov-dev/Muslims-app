7fd4b54f08eb72554757c8ce69f616a61a486ea2
Muhammadamin <webhacker0100@gmail.com>
2022-08-23 20:47:43 +0500

    Update

--- src/store/PrayTimes/index.js
+++ src/store/PrayTimes/index.js
@@ -36,6 +36,8 @@ export default {
          const { data } = await axios.get(`${process.env.VUE_APP_PRAY_TIMES_API}${$cookies.get('user_city')}&country=${$cookies.get('user_country')}=2&month=${new Date().getMonth() + 1}&year=${new Date().getFullYear()}`)
 
           const times = data.data[new Date().getDate() - 1].timings
+          
+          ctx.commit('setPrayTimes', [String(times['Fajr']), String(times['Sunrise']), String(times['Dhuhr']), String(times['Asr'], String(times['Maghrib']), String(times['Isha']))])
         }catch(error){
             console.log(error)
         }
