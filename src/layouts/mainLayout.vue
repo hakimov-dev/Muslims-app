b215d993bfe250736af302944f257f762e8406c5
Muhammadamin <webhacker0100@gmail.com>
2022-08-23 14:12:11 +0500

    Commented exes items

--- src/layouts/mainLayout.vue
+++ src/layouts/mainLayout.vue
@@ -107,13 +107,13 @@
             />
             <h1 class="font-medium text-xl">Muslims app</h1>
           </router-link>
-          <input
+          <!-- <input
             v-model="$store.state.searchValue"
             @keypress.enter="searchItems"
             class="md:block hidden search outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 w-[30%] p-2 px-10 bg-[#121416] rounded-[20px]"
             type="text"
             placeholder="Enter something for search..."
-          />
+          /> -->
           <div
             @click.stop
             :class="
