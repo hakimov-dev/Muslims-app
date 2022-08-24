703158a5608da4695847dd4dbd16ee8d3c0a3c22
Muhammadamin <webhacker0100@gmail.com>
2022-08-24 22:19:57 +0500

    Added v-model for search input

--- src/views/PrayTimes/index.vue
+++ src/views/PrayTimes/index.vue
@@ -4,6 +4,7 @@
       class="md:w-[60%] md:mx-auto md:flex justify-center search outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 w-[100%] p-2 px-10 bg-[#121416] rounded-[20px]"
       type="text"
       placeholder="Search city..."
+      v-model="$store.state.prayTimes.cityName"
     />
     <div
       class="pray-times-cards flex justify-evenly items-center flex-wrap mt-[3%]"
