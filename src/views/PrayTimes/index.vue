04f3f13ed845d6ce2ffa20343409805af7f6a758
Muhammadamin <webhacker0100@gmail.com>
2022-08-25 11:52:07 +0500

    Added comment

--- src/views/PrayTimes/index.vue
+++ src/views/PrayTimes/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pray-times">
-  <!-- Searched search api)) -->
+  <!-- TODO: Searched search api)) -->
     <!-- <input
       class="md:w-[60%] md:mx-auto md:flex justify-center search outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 w-[100%] p-2 px-10 bg-[#121416] rounded-[20px]"
       type="text"
