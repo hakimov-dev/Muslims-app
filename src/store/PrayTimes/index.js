925fd716558b2707a7d035a987a864d10bdfe0ea
Muhammadamin <webhacker0100@gmail.com>
2022-08-25 10:13:19 +0500

    Added API key

--- src/store/PrayTimes/index.js
+++ src/store/PrayTimes/index.js
@@ -52,7 +52,7 @@ export default {
       async searchCity(ctx, value){
         try{
             if(value !== ''){
-                const { data } = await axios.get(`${process.env.VUE_APP_PRAY_TIMES_DAY_API}${value}.json`)
+                const { data } = await axios.get(`${process.env.VUE_APP_PRAY_TIMES_DAY_API}${value}.json?key=${process.env.VUE_APP_PRAY_TIMES_DAY_API_KEY}`)
 
                 console.log(data)
             }else{
