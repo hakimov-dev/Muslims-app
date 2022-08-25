7b64f84bfac01c80453cfaa08f68e468cd975a62
Muhammadamin <webhacker0100@gmail.com>
2022-08-25 11:51:14 +0500

    Commented exes items

--- src/views/PrayTimes/index.vue
+++ src/views/PrayTimes/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="pray-times">
-    <input
+    <!-- <input
       class="md:w-[60%] md:mx-auto md:flex justify-center search outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 w-[100%] p-2 px-10 bg-[#121416] rounded-[20px]"
       type="text"
       placeholder="Search city..."
       v-model="$store.state.prayTimes.cityName"
       @keypress.enter="$store.dispatch('prayTimes/searchCity', $store.state.prayTimes.cityName.trim())"
-    />
+    /> -->
     <div
       class="pray-times-cards flex justify-evenly items-center flex-wrap mt-[3%]"
     >
