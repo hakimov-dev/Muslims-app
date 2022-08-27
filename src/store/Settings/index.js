e7c61ffe088022a68b00e2a6147ce80ad1405244
Muhammadamin <webhacker0100@gmail.com>
2022-08-27 21:11:29 +0500

    Added alert for notification

--- src/store/Settings/index.js
+++ src/store/Settings/index.js
@@ -13,6 +13,7 @@ export default {
         getUserAbouts(state, payload){
          if(state.user.username.trim() !== '' && state.user.name.trim() !== '' && state.user.surname.trim() !== '' || state.user.pic !== null){
             state.updateProfileError = ''
+            alert('Your profile was updated!')
             $cookies.set('user_username', state.user.username.trim())
             $cookies.set('user_lastname', state.user.surname.trim())
             $cookies.set('user_name', state.user.name.trim())
