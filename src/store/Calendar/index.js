aba92cf979ea9e7251a5202ed56efad28ff387a9
Muhammadamin <webhacker0100@gmail.com>
2022-08-27 10:42:31 +0500

    Created new request

--- src/store/Calendar/index.js
+++ src/store/Calendar/index.js
@@ -9,7 +9,7 @@ export default {
     actions: {
         async getCalendar(ctx, date){
           try{
-
+           const { data } = axios.get(`${process.env.VUE_APP_PRAY_TIMES_API}${$cookies.get('user_city')}&country=${$cookies.get('user_country')}=2&month=${date.month}&year=${date.year}`)
           }catch(error){
             console.log(error)
           }
