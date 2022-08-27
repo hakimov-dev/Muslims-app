9afc5cd87ccd651e51958941065c69f8ea82643a
Muhammadamin <webhacker0100@gmail.com>
2022-08-27 21:01:59 +0500

    Update cookies value

--- src/store/Settings/index.js
+++ src/store/Settings/index.js
@@ -13,6 +13,9 @@ export default {
         getUserAbouts(state, payload){
          if(state.user.username.trim() !== '' && state.user.name.trim() !== '' && state.user.surname.trim() !== '' || state.user.pic !== null){
             state.updateProfileError = ''
+            $cookies.setItem('user_username', state.user.username.trim())
+            $cookies.setItem('user_lastname', state.user.surname.trim())
+            $cookies.get('user_name', state.user.name.trim())
          }else
            state.updateProfileError = 'Rows are empty enter something in rows!'
         }
