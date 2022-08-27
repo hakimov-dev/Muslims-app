7d7e19f30ead5335cdaa993ad869c694df5cadaf
Muhammadamin <webhacker0100@gmail.com>
2022-08-27 21:03:10 +0500

    Added if condition and update user_pic cookie value

--- src/store/Settings/index.js
+++ src/store/Settings/index.js
@@ -16,6 +16,8 @@ export default {
             $cookies.setItem('user_username', state.user.username.trim())
             $cookies.setItem('user_lastname', state.user.surname.trim())
             $cookies.get('user_name', state.user.name.trim())
+            if(state.user.pic !== null)
+             $cookies.setItem('user_pic', state.user.pic)
          }else
            state.updateProfileError = 'Rows are empty enter something in rows!'
         }
