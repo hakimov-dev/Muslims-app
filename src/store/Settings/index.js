0337f6df7207fc963be849ee06c0366eb6242f9f
Muhammadamin <webhacker0100@gmail.com>
2022-08-27 21:08:15 +0500

    Errors found and corrected

--- src/store/Settings/index.js
+++ src/store/Settings/index.js
@@ -13,11 +13,11 @@ export default {
         getUserAbouts(state, payload){
          if(state.user.username.trim() !== '' && state.user.name.trim() !== '' && state.user.surname.trim() !== '' || state.user.pic !== null){
             state.updateProfileError = ''
-            $cookies.setItem('user_username', state.user.username.trim())
-            $cookies.setItem('user_lastname', state.user.surname.trim())
-            $cookies.setItem('user_name', state.user.name.trim())
+            $cookies.set('user_username', state.user.username.trim())
+            $cookies.set('user_lastname', state.user.surname.trim())
+            $cookies.set('user_name', state.user.name.trim())
             if(state.user.pic !== null)
-             $cookies.setItem('user_pic', state.user.pic)
+             $cookies.set('user_pic', state.user.pic)
          }else
            state.updateProfileError = 'Rows are empty enter something in rows!'
         }
