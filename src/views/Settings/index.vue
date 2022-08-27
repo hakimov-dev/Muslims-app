fdc3a583b5b9dd96d98b636e525a84dacdbefb99
Muhammadamin <webhacker0100@gmail.com>
2022-08-27 17:20:43 +0500

    Added name v-model

--- src/views/Settings/index.vue
+++ src/views/Settings/index.vue
@@ -44,6 +44,7 @@
           <input
             class="md:flex my-[30px] capitalize justify-center outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 w-[100%] p-2 px-4 bg-[#121416] rounded-[5px]"
             type="text"
+            v-model="$store.state.settings.user.name"
             placeholder="Enter your name..."
             maxlength="30"
           />
