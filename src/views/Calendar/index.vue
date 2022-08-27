61b68ce45afd8c0ce846d1a950245a91e40593bd
Muhammadamin <webhacker0100@gmail.com>
2022-08-27 13:59:07 +0500

    Deleted exes input

--- src/views/Calendar/index.vue
+++ src/views/Calendar/index.vue
@@ -2,14 +2,6 @@
   <div class="muslims-calendar md:p-[3%] md:pt-[1%]">
     <!-- Date picker -->
     <div class="flex justify-between items-center">
-      <!-- <input
-        v-model="$store.state.calendar.inputDate"
-        class="p-1 px-2 text-white outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 p-2 px-6 bg-[#121416] rounded-[10px] cursor-pointer"
-        type="date"
-        name="date"
-        id="date"
-        @change="Calendar($store.state.calendar.inputDate)"
-      /> -->
       <date-picker type="month" @change="Calendar($store.state.calendar.inputDate.trim())" v-model:value="$store.state.calendar.inputDate"></date-picker>
       <!-- <input
         class="md:mr-[1%] md:w-[30%] md:mx-auto md:flex justify-center search outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 w-[50%] p-2 px-10 bg-[#121416] rounded-[20px]"
