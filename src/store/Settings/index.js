d7216a04878e308933ad16723a585b854850a717
Muhammadamin <webhacker0100@gmail.com>
2022-08-27 18:09:22 +0500

    Added function content

--- src/store/Settings/index.js
+++ src/store/Settings/index.js
@@ -8,11 +8,13 @@ export default {
             pic: null 
         },
         updateProfileError: '',
-        updatePasswordError: ''
     },
     mutations: {
         getUserAbouts(state, payload){
-         
+         if(state.user.username.trim() !== '' && state.user.name.trim() !== '' && state.user.surname.trim() !== '' || state.user.pic !== null){
+            state.updateProfileError = ''
+         }else
+           state.updateProfileError = 'Rows are empty enter something in rows!'
         }
     },
     actions: {},
