e0e9bf29ccc48dfa15275afb679c47f1efa19a69
Muhammadamin <webhacker0100@gmail.com>
2022-08-27 14:27:46 +0500

    Update

--- src/views/Calendar/index.vue
+++ src/views/Calendar/index.vue
@@ -2,7 +2,7 @@
   <div class="muslims-calendar md:p-[3%] md:pt-[1%]">
     <!-- Date picker -->
     <div class="flex justify-between items-center">
-      <date-picker type="month" @change="Calendar($store.state.calendar.inputDate.trim())" v-model:value="$store.state.calendar.inputDate"></date-picker>
+      <date-picker type="month" @change="Calendar($store.state.calendar.inputDate)" v-model:value="$store.state.calendar.inputDate"></date-picker>
       <!-- <input
         class="md:mr-[1%] md:w-[30%] md:mx-auto md:flex justify-center search outline-none border-[1px] text-white transition-all font-medium focus:border-gray-300 hover:border-gray-300 focus:border-gray-300 border-gray-500 w-[50%] p-2 px-10 bg-[#121416] rounded-[20px]"
         type="text"
@@ -21,22 +21,17 @@ import 'vue-datepicker-next/index.css';
 import { useStore } from 'vuex'
 
 
-const newDate = new Date();
 const store = useStore()
 
 store.dispatch('prayTimes/getIpAdress')
 
-store.state.calendar.inputDate = `${newDate.getFullYear()}-${String(
-  newDate.getMonth() + 1
-).padStart(2, "0")}-${String(newDate.getDate()).padStart(2, "0")}`;
+store.state.calendar.inputDate = new Date();
 
 Calendar(store.state.calendar.inputDate)
 
 function Calendar(value){
  if(value !== ''){
-  const date = new Date(value)
-
-  store.dispatch('calendar/getCalendar', {month: date.getMonth() + 1, year: date.getFullYear()})
+  store.dispatch('calendar/getCalendar', {month: value.getMonth() + 1, year: value.getFullYear()})
  }
 }
 </script>
