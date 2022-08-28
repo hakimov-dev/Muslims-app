4ba3bef1263fd09763a39124d24067d9b9772634
Muhammadamin <webhacker0100@gmail.com>
2022-08-28 09:22:03 +0500

    Added function content

--- src/views/Settings/index.vue
+++ src/views/Settings/index.vue
@@ -92,7 +92,11 @@ function uploadImage(){
 }
 
 function stringify(obj) {
-  
+    const replacer = [];
+    for (const key in obj) {
+        replacer.push(key);
+    }
+    return JSON.stringify(obj, replacer);
 }
 </script>
 
